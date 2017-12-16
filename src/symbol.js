3119bd5b27851a0a44a4e4df39262fc85bb7f38d
Rohit Surya <33322975+RohitSurya@users.noreply.github.com>
2017-12-16 22:27:59 +0530

    Add files via upload

--- src/symbol.js
+++ src/symbol.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Image, Text, TouchableOpacity} from 'react-native';
-import {  Right, Button } from 'native-base';
+import {  Right, Container, Button, Fab } from 'native-base';
 import Icon from 'react-native-vector-icons/FontAwesome';
+
 export default class Symbol extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      active:true,
+    };
+  }
   render() {
     return (
-      <Right>
-      <Icon name="plus-circle"  size={72} color="#6ba3ff" underlayColor="transparent"/>
-      </Right>
+      <Container>
+      <Fab
+      active ={this.state.active}
+      style={{backgroundColor:'#6ba3ff',marginBottom:30}}
+      position="bottomRight"
+      onPress={() => this.setState({ active:!this.state.active})}>
+      <Icon name="plus-circle"  size={72} />
+      </Fab>
+      </Container>
     );
   }
 }
